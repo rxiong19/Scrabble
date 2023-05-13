9560ed66ca326106299bb922c1481e60c4fc9904
Elvira Xiong <47922282+rxiong19@users.noreply.github.com>
2023-05-12 23:18:22 -0400

    import express and morgan and create express app and port

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-import chalkAnimation from 'chalk-animation';
-import { database } from './database.js';
+import chalkAnimation from "chalk-animation";
+import { database } from "./database.js";
 // TODO #1: Import Morgan and Express
-
+import express from "express";
+import logger from "morgan";
 // TODO #2: Create an Express app.
-
+const app = express();
+const port = process.env.PORT || 3000;
 // TODO #3: Add middleware to the Express app.
 
 // TODO #4: Implement the /wordScore endpoint
@@ -17,7 +19,7 @@ import { database } from './database.js';
 // EVERYTHING BELOW THIS WILL WORK AFTER YOU IMPLEMENT THE ABOVE
 
 // This matches all routes that are not defined.
-app.all('*', async (request, response) => {
+app.all("*", async (request, response) => {
   response.status(404).send(`Not found: ${request.path}`);
 });
 
